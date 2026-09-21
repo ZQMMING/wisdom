376d9a1e5ee335b34d722ce5f6c9fae410ffba1a
Doubao-Ziping <ziping-bot@wisdom.shuntian>
2026-09-21 20:12:18 +0800

    chore: 更新run_case.py调试脚本

--- scripts/run_case.py
+++ scripts/run_case.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-import sys;sys.path.insert(0,'.')
-from engines.common.l0_fact_builder import build
+﻿# -*- coding: utf-8 -*-
+import sys
+sys.path.insert(0, '.')
+from engines.common.l0_fact_builder import build as l0build, WUXING
 from engines.common.daymaster_power_structure import build_power_structure
 from engines.common.daymaster_root_class import build_root_classes
 from engines.common.daymaster_tou_cang import build_tou_cang
@@ -9,37 +10,54 @@ from engines.common.daymaster_root_relations import build_root_relations
 from engines.common.daymaster_two_side import build_two_side
 from engines.common.daymaster_branch_tier import build_branch_tiers
 from engines.common.daymaster_tian_he import build_tian_he
+from engines.common.wuxing_power import build_wuxing_power, build_spectrum_topology, build_spectrum_from_power
 from engines.common.daymaster_power_network import build_power_network
-from engines.common.daymaster_power_queries import run_queries
+from engines.common.climate_structure import build_climate_structure
+from engines.common.special_pattern import build_special_patterns
+from engines.common.qtbj_climate_candidates import build_climate_candidates
+from engines.common.yongshen_engine import build_yongshen_engine
+from engines.common.dayun_xiji import build_dayun_xiji
 
-p={'year':['癸','亥'],'month':['壬','戌'],'day':['乙','卯'],'hour':['壬','午']}
-f=build(p)
-pa=build_power_structure(p)
-hst={p[k][1]:f['hidden_stems'][k] for k in ('year','month','day','hour')}
-rc=build_root_classes(p,hst);tc=build_tou_cang(f);wx=build_wang_xiang(f,f['day_stem'])
-rr=build_root_relations(rc,f['combination_facts']);ts=build_two_side(rc,tc,rr)
-bt=build_branch_tiers(p,f);th=build_tian_he(p,f)
-net=build_power_network(pa,rc,tc,wx,rr,ts,branch_tier=bt,tian_he=th,facts=f)
-qs=run_queries(net)
+K = ('year', 'month', 'day', 'hour')
+p = {'year': ['甲', '子'], 'month': ['辛', '未'], 'day': ['戊', '午'], 'hour': ['己', '未']}
+f = l0build(p)
+ds = f['day_stem']
+hst = {p[k][1]: f['hidden_stems'][k] for k in K}
+pa = build_power_structure(p)
+rc = build_root_classes(p, hst)
+tc = build_tou_cang(f)
+wxo = build_wang_xiang(f, ds)
+rr = build_root_relations(rc, f['combination_facts'])
+ts = build_two_side(rc, tc, rr)
+bt = build_branch_tiers(p, f)
+th = build_tian_he(p, f)
+wp = build_wuxing_power(p, f, th)
+net = build_power_network(pa, rc, tc, wxo, rr, ts, branch_tier=bt, tian_he=th, facts=f)
+net.setdefault('facts', {})['daymaster_element'] = WUXING[ds]
+sp = build_spectrum_topology(net, wp)
+_spd = build_spectrum_from_power(wp, p)
+sp['wang_shuai'] = _spd.get('wang_shuai')
+sp['qiang_ruo'] = _spd.get('qiang_ruo')
+clc = build_climate_candidates(f)
+cls = build_climate_structure(p, f, th)
+spp = build_special_patterns(p, f, wp, th, cls)
+ye = build_yongshen_engine(p, f, wp, sp, spp, clc)
 
-print('=== 1983-11-03 11:30 男 广东中山 ===')
-print('四柱: 癸亥 壬戌 乙卯 壬午')
-print('日主: 乙木')
+print('=== climate_stem_candidates ===')
+for c in ye.get('climate_stem_candidates', []):
+    print(' ', c)
 print()
-print('【算层】')
-print('  月令十神:', f.get('month_qi_ten_god',''))
-print('  月令藏干:', f['month_hidden_stems'])
-print('  透干:', f['month_transparent'])
-print()
-print('【辩层】')
-print('  根:', net['dimensions']['ROOT'])
-print('  月令:', net['dimensions']['SEASONAL'])
-print('  生扶:', net['dimensions']['SUPPORT'])
-print('  泄耗:', net['dimensions']['DRAIN'])
-print('  克制:', net['dimensions']['CONTROL'])
-print('  天干合:', net['dimensions']['TIAN_HE'])
-print()
-print('【断言层 SUPPORTED】')
-for q in qs:
-    if q['state']=='SUPPORTED':
-        print('  ✓', q['query_id'].split('QUERY-')[-1])
+
+dayun_list = ['壬申', '癸酉', '甲戌', '乙亥', '丙子', '丁丑', '戊寅', '己卯']
+dx = build_dayun_xiji(p, ye, dayun_list, {'wuxing_power': wp})
+per_step = dx.get('per_step', [])
+
+print('=== 大运喜忌 + stem_match_type ===')
+for i, step in enumerate(per_step):
+    gan = step.get('gan', '?')
+    zhi = step.get('zhi', '?')
+    ej = step.get('element_judgment', {})
+    smt = step.get('stem_match_type', '?')
+    age_start = 5 + i * 10
+    age_end = age_start + 9
+    print('%s%s (%d-%d岁): 元素级=%s | stem_match=%s' % (gan, zhi, age_start, age_end, ej.get('result','?'), smt))
